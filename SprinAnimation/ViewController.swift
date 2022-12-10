6f9debcd5650d329e8ac8f8869fdf2a6dc059cf0
MihailKisluhin <89089003390@mail.ru>
2022-12-10 13:40:55 +0400

    commit 1

--- SprinAnimation/ViewController.swift
+++ SprinAnimation/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet var animatedView: SpringView!
+    
+    @IBOutlet var firstParamLabel: UILabel!
+    @IBOutlet var secondParamLabel: UILabel!
+    @IBOutlet var thirdParamLabel: UILabel!
+    @IBOutlet var fourthParamLabel: UILabel!
+    @IBOutlet var fifthParamLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        presetParam()
     }
 
-
+    @IBAction func runAnimButton(_ sender: UIButton) {
+        animatedView.animate()
+        setRandomParam()
+    }
+    
+    func setRandomParam() {
+        animatedView.animation = "morph"
+        animatedView.force = 2
+        animatedView.duration = 2
+    }
+    
+    func presetParam() {
+        animatedView.animation = "wobble"
+        animatedView.force = 2
+        animatedView.duration = 2
+        
+        firstParamLabel.text = "Animation: \(animatedView.animation)"
+        secondParamLabel.text = "Force: \(animatedView.force)"
+    }
 }
 
